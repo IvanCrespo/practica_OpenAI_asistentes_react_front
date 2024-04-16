d6c8f7b2436b2887475f2275be28ab66b4f4acbd
IvanCrespo <contacto.ivancrespo@gmail.com>
2024-04-16 11:07:33 -0600

    Front Asistente AI Terminado

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -5,3 +5,7 @@ export * from './pros-cons-stream-generator.use-case';
 export * from './translate-text.use-case';
 export * from './text-to-audio.use-case';
 export * from './audio-to-text.use-case';
+export * from './image-generation.use-case';
+export * from './image-variation.use-case';
+export * from './assistant/create-thread.use-case';
+export * from './assistant/post-question.use-case';
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from "./orthography.interface";
 export * from './pros-cons.interface';
 export * from './translate.interface';
 export * from './audio-to-text.interface';
+export * from './assistant.interface';
